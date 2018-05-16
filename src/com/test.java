72e6646e455f2f0c6dd05f09e0ecb3c676a31f2a
gww <gww@163.com>
2018-05-16 20:37:45 +0800

    zzgq

--- src/com/test.java
+++ src/com/test.java
@@ -4,7 +4,8 @@ public class test {
 
 	public static void main(String[] args) {
 		System.out.println("zhang zong gong zhen shuai !");
-		System.out.println("guang guang love wei wei");
+		System.out.println("guang guang love cang lao shi");
+		System.out.println("zhang zong gong qiang !");
 	}
 
 }
